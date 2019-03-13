81e29e30fcc27bdf64bbcf72fbbf785e026e87ff
jenninmx <jenninmx@dukes.jmu.edu>
2019-03-13 17:01:22 -0400

    Linked IPWHOIS Output to Google Maps API as input

--- apiload.js
+++ apiload.js
@@ -1,32 +1,61 @@
 (function() {
   var httpRequest;
-  document.getElementById("submit").addEventListener('click', makeRequest);
+  document.getElementById("submit").addEventListener('click', IPWHOISRequest);
   
-  function makeRequest() {
+  function IPWHOISRequest() {
     var ipAddress = document.getElementsByName("ip")[0].value;
     httpRequest = new XMLHttpRequest();
 
     if (!httpRequest) {
-      alert('Giving up :( Cannot create an XMLHTTP instance');
+      alert('HTTP Request was denied.');
       return false;
     }
-    httpRequest.onreadystatechange = alertContents;
+    httpRequest.onreadystatechange = dataRead;
     httpRequest.open('GET', 'http://free.ipwhois.io/json/' + ipAddress);
     httpRequest.send();
   }
 
-  function alertContents() {
+  function dataRead() {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
       if (httpRequest.status === 200) {
         var parseRequest = JSON.parse(httpRequest.responseText);
 		document.getElementById("latitude").innerHTML = parseRequest.latitude;
 		document.getElementById("longitude").innerHTML = parseRequest.longitude;
+		document.getElementById("country").innerHTML = parseRequest.county;
+		document.getElementById("region").innerHTML = parseRequest.region;
+		document.getElementById("city").innerHTML = parseRequest.city;
+		
+		var lat = parseRequest.latitude;
+		var lng = parseRequest.longitude;
+		var latLng = new google.maps.LatLng(lat, lng);
+		
+		marker.setPosition(latLng);
       } else {
         alert('There was a problem with the request.');
       }
     }
   }
+  
   function newIp() {
 	var ipAddress = document.getElementsByName("ip").value;
   } 
+
 })();
+
+
+function initMap() {
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 2,
+	  draggable:false,
+      zoomControl: false,
+      center: {lat: 0,  lng: 0}
+    });
+
+    marker = new google.maps.Marker({
+                map:map,
+                animation: google.maps.Animation.DROP,
+                position: {lat: 0,  lng:  0}
+    });
+}
+
